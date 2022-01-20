41e9732323949656acaf507a26a59d43a85e212f
Sunny Kumar <sunny514173@gmail.com>
2022-01-20 20:09:51 +0530

    css module and scoped styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import * as headerStyle from './header.module.scss'
 
 const Header = () => {
     return (
@@ -7,7 +8,7 @@ const Header = () => {
             <h1>Hi my name is Sunny Kumar</h1>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
+                    <li><Link className={headerStyle.link} to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/blog">Blogs</Link></li>
