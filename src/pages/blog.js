40090dc3f9b83966421ed6f9d4500abe061e9484
Sunny Kumar <sunny514173@gmail.com>
2022-01-21 14:50:16 +0530

    listing blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery, useStatticQuery } from 'gatsby'
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            date
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
     return (
         <Layout>
             <h1>Welcome to my Blogs</h1>
-            <p>Blogs will be updated soon...</p>
+            <hr />
+            <ol>
+                {
+                    data.allMarkdownRemark.edges.map((edge) => {
+                        return (
+                            <li>
+                                <h3>{edge.node.frontmatter.title}</h3>
+                                <p>{edge.node.frontmatter.date}</p>
+                            </li>
+                        )
+                    })
+                }
+            </ol>
+            
         </Layout>
     )
 }
